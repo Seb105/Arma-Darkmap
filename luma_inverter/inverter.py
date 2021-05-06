efb05a2ebaa82202df9fe9ea40bfa99f7161d504
Seb <65898127+Seb105@users.noreply.github.com>
2021-05-06 23:22:16 +0100

    Formatting

--- luma_inverter/inverter.py
+++ luma_inverter/inverter.py
@@ -15,10 +15,12 @@ NEW_COLOUR_PATH_ACE = "..\\addons\\ace\\config_colours_patched.cpp"
 IMAGE_PATH = "conversion.png"
 TOML_PATH = "..\\.hemtt\\base.toml"
 
-def linear_conversion(oldMin,oldMax,oldVal,newMin,newMax):
-    oldRange = (oldMax - oldMin)  
-    newRange = (newMax - newMin)  
+
+def linear_conversion(oldMin, oldMax, oldVal, newMin, newMax):
+    oldRange = (oldMax - oldMin)
+    newRange = (newMax - newMin)
     return (((oldVal - oldMin) * newRange) / oldRange) + newMin
+
 
 def convert_colour(colour):
     colourHLS = list(colorsys.rgb_to_hls(*colour[0:3]))
@@ -28,17 +30,20 @@ def convert_colour(colour):
     colour = [round(v, 3) for v in list(colour)]
     return colour
 
+
 def pillow_colour(colour):
     return tuple([int(c*255) for c in copy.deepcopy(colour[0:3])])
 
+
 def main():
     timeTaken = time()
-    comparisonImg = Image.new(mode='RGB', size=(2560,2560), color=(255, 255, 255))
+    comparisonImg = Image.new(mode='RGB', size=(
+        2560, 2560), color=(255, 255, 255))
     draw = ImageDraw.Draw(comparisonImg)
     print("Reading old config")
     with open(OLD_COLOUR_PATH, "r") as f:
         lines = f.readlines()
-    print("Read old config in {0}s".format(round(time() - timeTaken,2)))
+    print("Read old config in {0}s".format(round(time() - timeTaken, 2)))
     timeTaken = time()
     print("Converting config lines")
     drawY = 0
@@ -46,7 +51,7 @@ def main():
     newlines = []
     for index, line in enumerate(lines):
         # Make sure we're dealing with a colour set
-        startTxt = line.replace(" ","")[:5]
+        startTxt = line.replace(" ", "")[:5]
         if "TAG::REMOVE" in line:
             continue
         if "class" in startTxt or "{" in startTxt[0] or "}" in startTxt[0]:
@@ -54,7 +59,8 @@ def main():
             continue
         if startTxt.find("color") == -1 or line.find("{") == -1:
             continue
-        colour = line[line.find("{") : line.find("}")+1].replace("{","[").replace("}","]")
+        colour = line[line.find("{"): line.find("}") +
+                      1].replace("{", "[").replace("}", "]")
         colour = ast.literal_eval(colour)
         if type(colour[0]) is str:
             continue
@@ -65,8 +71,8 @@ def main():
         colour.append(colourOld[3])
 
         # Drawing for debug
-        RGBColourOld =  pillow_colour(colourOld)
-        RGBColour =     pillow_colour(colour)
+        RGBColourOld = pillow_colour(colourOld)
+        RGBColour = pillow_colour(colour)
         draw.rectangle([(drawX, drawY), (drawX+15, drawY+16)], RGBColourOld)
         draw.rectangle([(drawX+16, drawY), (drawX+31, drawY+16)], RGBColour)
         if drawY >= 2560:
@@ -75,12 +81,13 @@ def main():
         else:
             drawY += 16
 
-        #construct newline
-        colour = str(colour).replace("[","{").replace("]","}").replace(" ","")
+        # construct newline
+        colour = str(colour).replace(
+            "[", "{").replace("]", "}").replace(" ", "")
         line = line[: line.find("{")]
         line = "{0}{1};\n".format(line, colour)
         newlines.append(line)
-    print("Converted config in {0}s".format(round(time() - timeTaken,2)))
+    print("Converted config in {0}s".format(round(time() - timeTaken, 2)))
     timeTaken = time()
 
     print("Removing unreferenced classes and simplifying empty classes")
@@ -89,15 +96,18 @@ def main():
         # Replace empty config brackets with simple declarations
         deleteLines = []
         for index, line in enumerate(newlines[:-1]):
-            startTxt = line.replace(" ","")[0]
-            startTxtNext =  newlines[index+1].replace(" ","")[0]
+            startTxt = line.replace(" ", "")[0]
+            startTxtNext = newlines[index+1].replace(" ", "")[0]
             if startTxt[0] == "{" and startTxtNext[0] == "}":
                 classLine = newlines[index-1]
                 strIndex = classLine.find("\n")
+                # Classes than inherit must have brackets
                 if len(classLine.strip().split(" ")) == 3:
-                    classLine = classLine[:strIndex] + " {};" + classLine[strIndex:]
+                    classLine = classLine[:strIndex] + \
+                        " {};" + classLine[strIndex:]
                 else:
-                    classLine = classLine[:strIndex] + ";" + classLine[strIndex:]
+                    classLine = classLine[:strIndex] + \
+                        ";" + classLine[strIndex:]
                 newlines[index-1] = classLine
                 deleteLines.append(index)
                 deleteLines.append(index+1)
@@ -106,24 +116,28 @@ def main():
             newlines[index] = ""
         newlines = [line for line in newlines if line != ""]
 
-
         # Remove classes that are not referenced and therefore not needed.
         deleteLines = []
         for index, line in enumerate(newlines):
             lineWords = line.strip().split(" ")
-            if lineWords[0] != "class": continue # Only interested in removing classes
-            classType = lineWords[1].replace(":","")
-            if line.find(";") == -1: continue # If line does not contain semicolon then the class contains data
+            if lineWords[0] != "class":
+                continue  # Only interested in removing classes
+            classType = lineWords[1].replace(":", "")
+            if line.find(";") == -1:
+                continue  # If line does not contain semicolon then the class contains data
             classType = classType.replace(";", "")
-            numOfReferences = len([l for l in newlines if ": "+classType+"\n" in l or ": "+classType+";" in l])
+            numOfReferences = len(
+                [l for l in newlines if ": "+classType+"\n" in l or ": "+classType+";" in l])
             if numOfReferences == 0:
                 deleteLines.append(index)
         for index in deleteLines:
             newlines[index] = ""
         newlines = [line for line in newlines if line != ""]
         iterations += 1
-        if deleteLines == []: break
-    print("Simplified classes in {0} iterations and {1}s".format(iterations, round(time() - timeTaken,2)))
+        if deleteLines == []:
+            break
+    print("Simplified classes in {0} iterations and {1}s".format(
+        iterations, round(time() - timeTaken, 2)))
     timeTaken = time()
 
     # Inheriting undeclared class warnings#
@@ -131,19 +145,24 @@ def main():
     warnings = []
     for index, line in enumerate(newlines):
         lineWords = line.strip().split(" ")
-        if lineWords[0] != "class": continue
-        if lineWords[1][-1] != ":": continue # Is class inheriting?
+        if lineWords[0] != "class":
+            continue
+        if lineWords[1][-1] != ":":
+            continue  # Is class inheriting?
         inheritance = lineWords[2].replace(";", "")
-        declarations = [line for line in newlines if line.find("class " + inheritance) != -1]
+        declarations = [line for line in newlines if line.find(
+            "class " + inheritance) != -1]
         if len(declarations) == 0:
-            warning = "WARNING: class {0} is inherited but never declared".format(inheritance)
-            if warning not in warnings: warnings.append(warning)
+            warning = "WARNING: class {0} is inherited but never declared".format(
+                inheritance)
+            if warning not in warnings:
+                warnings.append(warning)
     if len(warnings) == 0:
         print("No warnings")
     else:
         for warning in warnings:
             print(warning)
-    print("Checked for warnings in {0}s".format(round(time() - timeTaken,2)))
+    print("Checked for warnings in {0}s".format(round(time() - timeTaken, 2)))
     timeTaken = time()
 
     print("Writing new configs")
@@ -151,18 +170,20 @@ def main():
         f.writelines(newlines)
     with open(NEW_COLOUR_PATH_ACE, 'w') as f:
         f.writelines(newlines)
-    print("Wrote config in {0}s".format(round(time() - timeTaken,2)))
+    print("Wrote config in {0}s".format(round(time() - timeTaken, 2)))
     timeTaken = time()
 
     print("Saving preview image")
     comparisonImg.save(IMAGE_PATH)
-    print("Preview image made in in {0}s".format(round(time() - timeTaken,2)))
+    print("Preview image made in in {0}s".format(round(time() - timeTaken, 2)))
+
 
 def preview_pixel(pixel):
     pixel = [p/255 for p in pixel]
     newPixel = convert_colour(pixel)
     newPixel = pillow_colour(newPixel)
     return newPixel
+
 
 def preview_image():
     print("Building preview image")
@@ -175,12 +196,13 @@ def preview_image():
     img.save("map_preview_new.png")
     print("Done writing new image")
 
+
 def build_both_pbos():
     timeTaken = time()
     with open(TOML_PATH, 'r') as f:
         toml = f.readlines()
     for index, line in enumerate(toml):
-        toml[index] = line.replace("darkmap_ace","darkmap")
+        toml[index] = line.replace("darkmap_ace", "darkmap")
     with open(TOML_PATH, 'w') as f:
         f.writelines(toml)
     print("Building PBOs")
@@ -191,9 +213,10 @@ def build_both_pbos():
     with open(TOML_PATH, 'w') as f:
         f.writelines(toml)
     subprocess.call("..\\build_release_ace.bat")
-    print("Built in {0}s".format(round(time() - timeTaken,2)))
+    print("Built in {0}s".format(round(time() - timeTaken, 2)))
+
 
 if __name__ == '__main__':
     main()
     build_both_pbos()
-    #preview_image()
+    # preview_image()
